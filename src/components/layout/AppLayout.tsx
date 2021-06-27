9e7f8e81e2e65183a2e7dedc9d85a48441aadd4d
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-27 09:47:21 +0700

    fixed layout and adding home screen, library screen

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,8 +1,7 @@
 import SideBar from "../sidebar/SideBar";
-import { links } from "../links";
-import { optionsListProfile } from "../optionsListProfile";
 import Meta from "../site/Meta";
 import Header from "../header/Header";
+import { useState } from "react";
 
 interface Props {
   title: string;
@@ -23,16 +22,14 @@ const AppLayout = ({ title, desc, children }: Props) => {
     },
   ];
 
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div>
       <Meta pageTitle={title} description={desc} />
       <main className="bg-gray-100 dark:bg-gray-800 rounded-2xl h-screen overflow-hidden relative">
         <div className="flex items-start justify-between">
-          <div className="h-screen hidden lg:block my-4 ml-1 shadow-lg relative w-80">
-            <div className="bg-white h-full rounded-2xl dark:bg-gray-700">
-              <SideBar />
-            </div>
-          </div>
+          <SideBar isHide={false} />
           <div className="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
             <Header />
             <div className="overflow-auto h-screen pb-24 pt-2 pr-2 pl-2 md:pt-0 md:pr-0 md:pl-0">
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,42 +1,19 @@
+import SimpleLevelsCard from "../components/card/SimpleLevelsCard";
 import AppLayout from "../components/layout/AppLayout";
-import GoogleTask from "../components/kit/components/elements/data/GoogleTask";
-import SlackTask from "../components/kit/components/elements/data/SlackTask";
-import TasksList from "../components/kit/components/elements/data/TasksList";
-import LessonsList from "../components/kit/components/elements/data/LessonsList";
-import CalendarCardMonth from "../components/kit/components/elements/data/CalendarCardMonth";
-import MessagesList from "../components/kit/components/elements/data/MessagesList";
 
 const home = () => {
   return (
     <div>
       <AppLayout title="home" desc="home">
-        <div>
-          <div className="flex flex-col flex-wrap sm:flex-row ">
-            <div className="w-full sm:w-1/2 xl:w-1/3">
-              <div className="mb-4">
-                <GoogleTask />
-              </div>
-              <div className="mb-4">
-                <SlackTask />
-              </div>
-            </div>
-
-            <div className="w-full sm:w-1/2 xl:w-1/3">
-              <div className="mb-4 mx-0 sm:ml-4 xl:mr-4">
-                <TasksList />
-              </div>
-              <div className="mb-4 sm:ml-4 xl:mr-4">
-                <LessonsList />
-              </div>
-            </div>
-            <div className="w-full sm:w-1/2 xl:w-1/3">
-              <div className="mb-4">
-                <CalendarCardMonth />
-              </div>
-              <div className="mb-4">
-                <MessagesList />
-              </div>
-            </div>
+        <div className="overflow-auto h-screen pb-24 px-4 md:px-6">
+          <h1 className="text-4xl font-semibold text-gray-800 dark:text-white">
+            Overview
+          </h1>
+          <h2 className="text-md text-gray-400">latest</h2>
+          <div className="flex bg-white">
+            <SimpleLevelsCard title="PRJ301" learning={54} notStudied={120} />
+            <SimpleLevelsCard title="PRJ301" learning={54} notStudied={120} />
+            <SimpleLevelsCard title="PRJ301" learning={54} notStudied={120} />
           </div>
         </div>
       </AppLayout>
