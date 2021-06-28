112965518c4316680192705e9f57732180c236cc
Nguyễn Thái Dương <loswkpq17@gmail.com>
2021-06-28 15:55:35 +0700

    fixed fetch data method

--- src/redux/actions/authAction.ts
+++ src/redux/actions/authAction.ts
@@ -1,3 +1,20 @@
 import { Dispatch } from 'redux'
 import { AUTH, IAuthType } from '../types/authType'
 import { ALERT, IAlertType } from '../types/alertType'
+
+import { IUserLogin, IUserRegister } from '../../utils/TypeScript'
+import { postAPI, getAPI } from '../../utils/FetchData'
+
+export const register = (userRegister: IUserRegister) => 
+async (dispatch: Dispatch<IAuthType | IAlertType>) => {
+  
+  try {
+    dispatch({ type: ALERT, payload: { loading: true } })
+    
+    const res = await postAPI('register', userRegister)
+
+    dispatch({ type: ALERT, payload: { success: res.data.msg } })
+  } catch (err: any) {
+    dispatch({ type: ALERT, payload: { errors: err.response.data.msg } })
+  }
+}
--- src/utils/FetchData.ts
+++ src/utils/FetchData.ts
@@ -1,35 +1,44 @@
 import axios from 'axios'
 
 
-export const postAPI = async (url: string, post: object, token?:string) => {
-  const res = await axios.post(`/api/${url}`, post, {
-    headers: { Authorization: token }
-  })
-
-  return res;
+export const postAPI = async (url: string, body: object) => {
+  const access_token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  };
+  return axios.post(url, body, config);
 }
 
 
-export const getAPI = async (url: string, token?:string) => {
-  const res = await axios.get(`/api/${url}`, {
-    headers: { Authorization: token }
-  })
+export const getAPI = async (url: string) => {
 
-  return res;
+  const access_token = localStorage.getItem("access-token");
+  const config = {
+    headers: {
+        Authorization: `Bearer ${access_token}`
+    }
+  };
+  return axios.get(url, config);
 }
 
-export const putAPI = async (url: string, token?:string) => {
-    const res = await axios.put(`/api/${url}`, {
-      headers: { Authorization: token }
-    })
-  
-    return res;
+export const putAPI = async (url: string, body: object) => {
+  const access_token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  };
+  return axios.put(url, body, config);
 }
 
-export const deleteAPI = async (url: string, token?:string) => {
-    const res = await axios.delete(`/api/${url}`, {
-      headers: { Authorization: token }
-    })
-  
-    return res;
+export const deleteAPI = async (url: string) => {
+  const access_token = localStorage.getItem("access-token");
+  const config = {
+    headers: {
+        Authorization: `Bearer ${access_token}`
+    }
+  };
+  return axios.delete(url, config);
 }
