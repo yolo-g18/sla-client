2981ef3883f3611d7879ac785b2dcab9879b12f8
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-08-01 11:34:32 +0700

    fix css inviting guest

--- src/components/layout/RoomLayout.tsx
+++ src/components/layout/RoomLayout.tsx
@@ -1274,9 +1274,9 @@ const RoomLayout = (props: Props) => {
                   <br></br>
                   <form onSubmit={searchUser} className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                     <div className=" relative ">
-                      <input type="text" id="textNameUserSearch" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="username" />
+                      <input type="text" id="textNameUserSearch" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:border-transparent" placeholder="username" />
                     </div>
-                    <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
+                    <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-green-500 rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 " type="submit">
                       Search
                     </button>
                   </form>
