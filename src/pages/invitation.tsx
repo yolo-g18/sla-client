89425d873aef95f56be01c612f2649562877f5bd
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-08-02 13:51:34 +0700

    title of invitation

--- src/pages/invitation.tsx
+++ src/pages/invitation.tsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { PARAMS } from "../common/params";
 import AppLayout from "../components/layout/AppLayout";
 import { ALERT } from "../redux/types/alertType";
-import { getAPI,deleteAPI,putAPI } from "../utils/FetchData";
+import { getAPI, deleteAPI, putAPI } from "../utils/FetchData";
 import { IHostInvitation } from "../utils/TypeScript";
 import { RootStore } from "../utils/TypeScript";
 import Snackbar from "@material-ui/core/Snackbar";
@@ -25,8 +25,8 @@ const invitation = () => {
   const [typeToast, setTypeToast] = React.useState("success");
   const [messageToast, setMessageToast] = React.useState("");
 
-   //handel close toast
-   const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+  //handel close toast
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
     if (reason === "clickaway") {
       return;
     }
@@ -41,7 +41,7 @@ const invitation = () => {
         dispatch({ type: ALERT, payload: { loading: true } });
         const res = await getAPI(`${PARAMS.ENDPOINT}room/getRoomInvitationList`);
         setInvitationList(res.data);
-       
+
         dispatch({ type: ALERT, payload: { loading: false } });
 
 
@@ -54,25 +54,25 @@ const invitation = () => {
     excute();
   }, [alert.success]);
 
-  async function acceptInvitation(room_id:number){
+  async function acceptInvitation(room_id: number) {
 
     const data = {
-      "room_id":room_id,
-      "member_id":auth.userResponse?._id
+      "room_id": room_id,
+      "member_id": auth.userResponse?._id
     }
 
     dispatch({ type: ALERT, payload: { loading: true } });
     try {
       const res = await putAPI(
-        `${PARAMS.ENDPOINT}room/addMemberToRoom`,data
+        `${PARAMS.ENDPOINT}room/addMemberToRoom`, data
       );
 
-    dispatch({ type: ALERT, payload: { loading: false } });
-      
+      dispatch({ type: ALERT, payload: { loading: false } });
+
     } catch (err) {
       dispatch({ type: ALERT, payload: { loading: false } });
 
- 
+
     }
 
     deleteRequest(room_id);
@@ -81,17 +81,17 @@ const invitation = () => {
     setMessageToast("invitation accepted");
     setTypeToast("success");
     setIsToastOpen(true);
-  
+
   }
 
-  function handleRejectInvitation(room_id:number){
+  function handleRejectInvitation(room_id: number) {
     deleteInvitaion(room_id);
     setMessageToast("invitation rejected");
     setTypeToast("success");
     setIsToastOpen(true);
 
   }
-  async function deleteRequest(room_id:number){
+  async function deleteRequest(room_id: number) {
 
     dispatch({ type: ALERT, payload: { loading: true } });
     try {
@@ -99,16 +99,16 @@ const invitation = () => {
         `${PARAMS.ENDPOINT}room/deleteRoomRequestAttend/${room_id}/${auth.userResponse?._id}`
       );
 
-    dispatch({ type: ALERT, payload: { loading: false } });
-      
+      dispatch({ type: ALERT, payload: { loading: false } });
+
     } catch (err) {
       dispatch({ type: ALERT, payload: { loading: false } });
 
- 
+
     }
   }
 
-  async function deleteInvitaion(room_id:number){
+  async function deleteInvitaion(room_id: number) {
 
     dispatch({ type: ALERT, payload: { loading: true } });
     try {
@@ -116,19 +116,29 @@ const invitation = () => {
         `${PARAMS.ENDPOINT}room/deleteRoomInvitation/${room_id}/${auth.userResponse?._id}`
       );
 
-    dispatch({ type: ALERT, payload: { loading: false ,success:"ss"} });
-      
+      dispatch({ type: ALERT, payload: { loading: false, success: "ss" } });
+
     } catch (err) {
       dispatch({ type: ALERT, payload: { loading: false } });
 
- 
+
     }
   }
   return (
     <AppLayout title="INVITATION" desc="INVITATION">
+      {invitationList.length > 0 ? (
+        invitationList.length === 1 ? (
+          <p className="text-lg font-bold text-gray-700 m-16">You received {invitationList.length} invitation</p>)
+          : (
+            <p className="text-lg font-bold text-gray-700 m-16">You received {invitationList.length} invitations</p>
+          )
+      ) :
+        (
+          <p className="text-lg font-bold text-gray-700 m-16">You have no invitation</p>)
+      }
       {invitationList.map((item, index) => {
-          return (
-            <div key={index} className="md:w-1/5 sm:w-full rounded-lg shadow-lg bg-white my-3">
+        return (
+          <div key={index} className="md:w-1/5 sm:w-full rounded-lg shadow-lg bg-white my-3">
             <div className="flex justify-between border-b border-gray-300 px-10 py-4">
               <div>
                 <i className="fa fa-exclamation-triangle text-orange-500"></i>
@@ -144,26 +154,26 @@ const invitation = () => {
               <br></br>
               <p className="text-xs italic">{item.timeInvited}</p>
             </div>
-           
+
             <div className="px-5 py-4 flex justify-end">
-              <button onClick={() => acceptInvitation(item.roomId)}  className="bg-green-500 text-white w-28 py-1  mx-4 rounded-md text-sm font-medium hover:bg-green-600">Accept</button>
+              <button onClick={() => acceptInvitation(item.roomId)} className="bg-green-500 text-white w-28 py-1  mx-4 rounded-md text-sm font-medium hover:bg-green-600">Accept</button>
               <button onClick={() => handleRejectInvitation(item.roomId)} className="bg-gray-100 border-2 text-gray-700 w-28 py-1 mx-4 rounded-md text-sm font-medium hover:bg-gray-300">Reject</button>
             </div>
           </div>
-          )
-        })}
-              <Snackbar
-          open={isToastOpen}
-          autoHideDuration={6000}
+        )
+      })}
+      <Snackbar
+        open={isToastOpen}
+        autoHideDuration={6000}
+        onClose={handleClose}
+      >
+        <Alert
           onClose={handleClose}
+          severity={typeToast === "success" ? "success" : "error"}
         >
-          <Alert
-            onClose={handleClose}
-            severity={typeToast === "success" ? "success" : "error"}
-          >
-            {messageToast}
-          </Alert>
-        </Snackbar>
+          {messageToast}
+        </Alert>
+      </Snackbar>
 
     </AppLayout>
   )
