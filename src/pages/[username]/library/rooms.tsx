af7a3904d4f8fd94e3c51f6bf380c492d0c6dffd
khanhngocc <khanhnnhe130816@fpt.edu.vn>
2021-08-15 23:08:07 +0700

    fix fix

--- src/pages/[username]/library/rooms.tsx
+++ src/pages/[username]/library/rooms.tsx
@@ -178,7 +178,7 @@ const rooms = (props: any) => {
             </div>
           ))
         )}
-        {}
+       
         {isShowRemoveModal ? (
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 backdrop-filter backdrop-brightness-50 -mt-12">
             <div className="h-screen w-full absolute flex items-center justify-center bg-modal">
